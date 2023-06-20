6e28f9b5c3ae8ff8ee353c185209480ce1696416
Nedas Kibildis <nedaskibildis@Nedass-MacBook-Air.local>
2023-06-20 14:59:54 -0400

    Finishing Up Basic Styles, just need to do edit form and show page

--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000)
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, porro repellendus praesentium voluptate architecto esse maiores a reprehenderit facere corrupti, obcaecati quis sequi tempore, eius aliquam culpa ad molestias mollitia.',
+            price
         })
         await camp.save();
     }

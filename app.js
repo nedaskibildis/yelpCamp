8549a09e53bdbbf46485befaae3616bb50a7f2f1
Nedas Kibildis <nedaskibildis@Nedass-MacBook-Air.local>
2023-07-04 13:46:20 -0400

    Added Flash Messages For Successful creation and updates and errors

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressErrors')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 const campgrounds = require('./routes/campground.js')
 const reviews = require('./routes/reviews.js')
@@ -24,7 +26,26 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
 
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
+app.use(flash())
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error')
+    next();
+})
 
 app.use("/campgrounds", campgrounds)
 app.use("/campgrounds/:id/reviews", reviews)
--- routes/campground.js
+++ routes/campground.js
@@ -28,12 +28,17 @@ router.get('/new', (req, res) => {
 router.post('/', validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
+    req.flash('success', 'Successfully made a new campground')
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 router.get('/:id', catchAsync (async (req, res) => {
     const {id} = req.params;
     const campground = await Campground.findById(id).populate('reviews');
+    if (!campground) {
+        req.flash('error', 'Could Not Find Campground')
+        return res.redirect('/campgrounds')
+    }
     res.render('campgrounds/show', {campground})
 }))
 
@@ -44,12 +49,14 @@ router.get('/:id/edit', catchAsync (async (req, res) => {
 
 router.put('/:id', validateCampground, catchAsync (async (req, res) => {
     const campground = await Campground.findByIdAndUpdate(req.params.id, {...req.body.campground})
+    req.flash('success', 'Successfully Updated Campground!')
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 router.delete('/:id', catchAsync (async (req, res) => {
     const {id} = req.params;
     const campground = await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully Deleted Campground')
     res.redirect('/campgrounds')
 }))
 

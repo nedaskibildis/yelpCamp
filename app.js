3b9bb23b5d7c6ebf457c544593dff2c79ef0a8dd
Nedas Kibildis <nedaskibildis@Nedass-MacBook-Air.local>
2023-07-13 16:58:17 -0400

    finished yelp camp

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campground')
 const reviewRoutes = require('./routes/reviews')
 
-const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp"
+// process.env.DB_URL || 
+const dbUrl = "mongodb://localhost:27017/yelp-camp"
 const secret = process.env.SECRET || 'thisshouldbeasecret'
 mongoose.connect(dbUrl)
 
